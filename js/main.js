d64713710dab9b0326ec053080bbba36f69ae76c
ElvisBemol <elviscarlosdeo@gmail.com>
2020-11-04 17:37:20 -0300

    main.js

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var nome = "Elvis Carlos";
+var idade = "41";
+var idade2 = "10";
+var frase = "Brasil tem a melhor educação do mundo";
+//alert("Bem vindo " + nome);
+//alert(idade + idade2);
+//alert(frase.replace("Brasil", "Japão"));
+alert(frase.toLowerCase());
